37065751670715d505ce8da68f7fe02d2cb9f92d
MUNICIPALIDAD\mvelez <allcompva@gmail.com>
2023-01-27 12:57:26 -0300

    no message

---
+++ Web_Institucional_Api/Entities/Componentes/MenuMobile.cs
@@ -0,0 +1,21 @@
+﻿namespace Web_Institucional_Api.Entities.Componentes
+{
+    public class MenuMobile
+    {
+        public static string get()
+        {
+            string html =
+                @"
+
+                ";
+
+            return html;
+        }
+        public static string getLogo()
+        {
+            string html = string.Empty;
+
+            return html;
+        }
+    }
+}
